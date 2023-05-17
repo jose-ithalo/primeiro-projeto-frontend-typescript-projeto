628e5e21b9c9c86ef7b2243cb4f3915b8875d19f
José Ithalo <jose_ithalo@hotmail.com>
2023-05-17 16:11:15 -0300

    Criação de página SignIn

---
+++ base/src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import './signIn.css';
+import '../../css/responsive.css';
+
+function SignIn() {
+    return (
+        <div className='container'>
+            <div className='cardSignIn'></div>
+        </div>
+    )
+}
+
+export default SignIn;
