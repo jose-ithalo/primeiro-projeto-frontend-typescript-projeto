b293e00885f8d1009ca0dac44b75b3322588ddce
José Ithalo <jose_ithalo@hotmail.com>
2023-05-18 20:20:28 -0300

    Integração da API realizada

--- base/src/pages/SignIn/index.tsx
+++ base/src/pages/SignIn/index.tsx
@@ -4,15 +4,44 @@ import '../../css/responsive.css';
 import LogoCubos from '../../assets/logoCubos.svg';
 
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import { FormEvent, useState } from 'react';
 
 function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(evt: FormEvent) {
+        evt.preventDefault();
+
+        try {
+
+            if (!email || !password) {
+                throw new Error('Preecha todos os campos');
+            }
+
+            const response = await api.post('/login', {
+                email,
+                password
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className='container'>
             <div className='cardSignIn'>
                 <img src={LogoCubos} alt='Logo da Cubos' />
-                <form className='formCubos'>
-                    <input type='text' placeholder='E-mail' />
-                    <input type='password' placeholder='Passaword' />
+
+                <form className='formCubos' onSubmit={handleSubmit}>
+                    <input type='email' placeholder='E-mail' value={email}
+                        onChange={(evt) => setEmail(evt.target.value)} />
+
+                    <input type='password' placeholder='Passaword' value={password}
+                        onChange={(evt) => setPassword(evt.target.value)} />
 
                     <span>Não tem cadastro? <Link to='/'>Clique aqui!</Link></span>
 
