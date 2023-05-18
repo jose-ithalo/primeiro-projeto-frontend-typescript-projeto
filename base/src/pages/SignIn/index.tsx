fb0a7e84ae73c9e78e8d7fb5affbb77a1fd9ea8c
José Ithalo <jose_ithalo@hotmail.com>
2023-05-18 11:22:05 -0300

    Desenvolvimento do formulário

--- base/src/pages/SignIn/index.tsx
+++ base/src/pages/SignIn/index.tsx
@@ -1,10 +1,24 @@
 import './signIn.css';
 import '../../css/responsive.css';
 
+import LogoCubos from '../../assets/logoCubos.svg';
+
+import { Link } from 'react-router-dom';
+
 function SignIn() {
     return (
         <div className='container'>
-            <div className='cardSignIn'></div>
+            <div className='cardSignIn'>
+                <img src={LogoCubos} alt='Logo da Cubos' />
+                <form>
+                    <input type='text' placeholder='E-mail' />
+                    <input type='password' placeholder='Passaword' />
+
+                    <span>Não tem cadastro? <Link to='/'>Clique aqui!</Link></span>
+
+                    <button>Login</button>
+                </form>
+            </div>
         </div>
     )
 }
