05d403c1236d9047b38be893fb4e8a0afdbc6610
José Ithalo <jose_ithalo@hotmail.com>
2023-05-21 22:18:19 -0300

    Adicição de rotas protegidas

---
+++ base/src/routes.tsx
@@ -0,0 +1,33 @@
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import SignIn from './pages/SignIn';
+import useAuth from './hook/useAuth';
+
+type Prop = {
+    redirectTo: string
+}
+
+function ProtectedRoutes({ redirectTo }: Prop) {
+
+    const { handleGetToken } = useAuth();
+
+    return handleGetToken() ? <Outlet /> : <Navigate to={redirectTo} />
+
+}
+
+function MainRoutes() {
+    return (
+        <Routes>
+            <Route path='/'>
+                <Route path='/' element={<SignIn />} />
+                <Route path='/SignIn' element={<SignIn />} />
+            </Route>
+
+            <Route element={<ProtectedRoutes redirectTo='/' />}>
+                <Route path='/Main' element={<Main />} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default MainRoutes;
