c6cab7bd82ce1518d2df3ced0e6bf6c841e42456
José Ithalo <jose_ithalo@hotmail.com>
2023-05-23 16:54:33 -0300

    Componente TeacherCard inserido à Main

--- base/src/pages/Main/index.tsx
+++ base/src/pages/Main/index.tsx
@@ -1,11 +1,17 @@
 import './main.css';
 
 import Header from '../../components/Header';
+import TeacherCard from '../../components/TeacherCard';
 
 function Main() {
   return (
     <div className='container'>
       <Header />
+      <TeacherCard teacher={{
+        id: 1,
+        avatar: 'https://static.vecteezy.com/system/resources/thumbnails/009/749/751/small/avatar-man-icon-cartoon-male-profile-mascot-illustration-head-face-business-user-logo-free-vector.jpg',
+        name: 'Desconhecido'
+      }} />
     </div>
   );
 }
