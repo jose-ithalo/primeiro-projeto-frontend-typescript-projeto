0b9a95b9db7fb2dc21661d05e163e20941e227a4
José Ithalo <jose_ithalo@hotmail.com>
2023-05-26 21:55:56 -0300

    Atribuição de elementos no card de detalhes

--- base/src/pages/TeacherDetail/index.tsx
+++ base/src/pages/TeacherDetail/index.tsx
@@ -10,7 +10,18 @@ function TeacherDeatail() {
             <div className="cardDetail">
                 <Avatar
                     image='https://static.vecteezy.com/system/resources/thumbnails/009/749/751/small/avatar-man-icon-cartoon-male-profile-mascot-illustration-head-face-business-user-logo-free-vector.jpg' />
+                <h1>Exemplo</h1>
+                <span>BackEnd</span>
 
+                <div></div>
+
+                <h2>Bio</h2>
+
+                <p>
+                    alçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdkls alçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;
+                    sfjçasfdklsalçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdkls alçsfaçsfjal;sfjçasfdkls
+                    alçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdkls
+                </p>
             </div>
         </div>
     )
