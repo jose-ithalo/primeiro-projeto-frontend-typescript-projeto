d8b734cea4cc576aadc2d63bc3abe7f4aaeca84f
José Ithalo <jose_ithalo@hotmail.com>
2023-05-28 17:00:20 -0300

    Adição de atributo para definir tamanho

--- base/src/pages/TeacherDetail/index.tsx
+++ base/src/pages/TeacherDetail/index.tsx
@@ -9,18 +9,18 @@ function TeacherDeatail() {
             <Header showArrow={true} />
             <div className="cardDetail">
                 <Avatar
-                    image='https://static.vecteezy.com/system/resources/thumbnails/009/749/751/small/avatar-man-icon-cartoon-male-profile-mascot-illustration-head-face-business-user-logo-free-vector.jpg' />
+                    image='https://static.vecteezy.com/system/resources/thumbnails/009/749/751/small/avatar-man-icon-cartoon-male-profile-mascot-illustration-head-face-business-user-logo-free-vector.jpg'
+                    size={170}
+                />
                 <h1>Exemplo</h1>
                 <span>BackEnd</span>
 
-                <div></div>
+                <div className='horizontalLine'></div>
 
                 <h2>Bio</h2>
 
                 <p>
-                    alçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdkls alçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;
-                    sfjçasfdklsalçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdkls alçsfaçsfjal;sfjçasfdkls
-                    alçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdklsalçsfaçsfjal;sfjçasfdkls
+                    Olá, sou Daniel, mas pode me chamar de Dani. Acredito que a vida só faz sentido quando você descobre quem é e para onde está indo e vejo, na Educação, o caminho mais curto para encontrar este sentido.
                 </p>
             </div>
         </div>
