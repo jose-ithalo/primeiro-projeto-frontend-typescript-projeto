f411b2793bc3f2590ef8f461d56abd2a85e9bbdf
José Ithalo <jose_ithalo@hotmail.com>
2023-05-28 14:01:53 -0300

    Importação css para responsividade

--- base/src/index.tsx
+++ base/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './css/global.css';
 import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from './routes';
+import './css/responsive.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
