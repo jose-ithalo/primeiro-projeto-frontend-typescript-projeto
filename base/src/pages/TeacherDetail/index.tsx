54956988376b3a686e84945a29d92eed0797474a
José Ithalo <jose_ithalo@hotmail.com>
2023-05-30 16:20:09 -0300

    Aplicação de valores dinâmicos ao arquivo

--- base/src/pages/TeacherDetail/index.tsx
+++ base/src/pages/TeacherDetail/index.tsx
@@ -1,27 +1,27 @@
 import './detail.css';
 
-import Header from "../../components/Header";
 import Avatar from '../../components/Avatar';
+import Header from "../../components/Header";
+import useDetail from '../../hooks/useDetail';
 
 function TeacherDeatail() {
+
+    const { selectedTeacher } = useDetail();
+
     return (
         <div className="container">
             <Header showArrow={true} />
             <div className="cardDetail">
-                <Avatar
-                    image='https://static.vecteezy.com/system/resources/thumbnails/009/749/751/small/avatar-man-icon-cartoon-male-profile-mascot-illustration-head-face-business-user-logo-free-vector.jpg'
-                    size={170}
-                />
-                <h1>Exemplo</h1>
-                <span>BackEnd</span>
+                <Avatar image={selectedTeacher?.avatar!} size={170} />
+
+                <h1>{selectedTeacher?.name}</h1>
+                <span>{selectedTeacher?.stack}</span>
 
                 <div className='horizontalLine'></div>
 
                 <h2>Bio</h2>
 
-                <p>
-                    Olá, sou Daniel, mas pode me chamar de Dani. Acredito que a vida só faz sentido quando você descobre quem é e para onde está indo e vejo, na Educação, o caminho mais curto para encontrar este sentido.
-                </p>
+                <p>{selectedTeacher?.bio}</p>
             </div>
         </div>
     )
