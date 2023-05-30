3461c632650237092291cac0526bff8acfa952cc
José Ithalo <jose_ithalo@hotmail.com>
2023-05-30 16:22:13 -0300

    Mudança da função para obter professor para um estado

--- base/src/hooks/useDetail.ts
+++ base/src/hooks/useDetail.ts
@@ -1,25 +1,25 @@
 import Teacher from "../types/teacherType";
 
+import { useState } from 'react';
+
 function useDetail() {
+
+    const [selectedTeacher, setSelectedTeacher] = useState<Teacher>(
+        localStorage.getItem('current-teacher')
+            ? JSON.parse(localStorage.getItem('current-teacher')!) :
+            { id: 0, name: '', avatar: '', bio: '', stack: '' }
+    );
 
     function handleInsertTeacher(teacher: Teacher): void {
 
         localStorage.setItem('current-teacher', JSON.stringify(teacher));
+        setSelectedTeacher(teacher);
 
-    }
-
-    function handleGetTeacher(): Teacher | null {
-
-        const currentTeacher = localStorage.getItem('current-teacher') ?
-            JSON.parse(localStorage.getItem('current-teacher')!) :
-            null
-
-        return currentTeacher;
     }
 
     return {
         handleInsertTeacher,
-        handleGetTeacher
+        selectedTeacher
     }
 
 }
